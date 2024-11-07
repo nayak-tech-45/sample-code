d16ca27b089d39c4b8806fcefff3c09b2056c210
unknown <Anil Kumar Behera>
2024-11-07 12:34:48 +0530

    adding a new file

---
+++ sample_python_code2.py
@@ -0,0 +1,8 @@
+marks = int(input("enter the marks:"))
+if marks >= 35:
+    if marks >= 80:
+        print("A grade")
+    else:
+        print("you passed but you did not secure A grade")
+else:
+    print("fail")
